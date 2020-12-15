296495dc072591c364d2c5565f19e711b2a5faef
AndreyM <andrey.mikhailov@taskdata.ru>
2020-12-15 15:28:48 +0300

    algorithm TwoSum from LeetCode

---
+++ src/LeetCode/TwoSum.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] result = twoSum(new int[]{2, 5, 5, 11}, 10);
+        for (int j : result)
+            System.out.println(j);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            int tmp = 0;
+
+            for (int j = i+1; j < nums.length; j++) {
+                tmp = j;
+                if ((nums[i] + nums[j]) == target) {
+                    break;
+                }
+            }
+            if ((nums[i] + nums[tmp]) == target) {
+                result = new int[]{i, tmp};
+                break;
+            }
+        }
+        return result;
+    }
+}
