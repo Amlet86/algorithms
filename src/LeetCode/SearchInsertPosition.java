c28c87a88593f8332202ab929da440886f0cc482
AndreyM <andrey.mikhailov@taskdata.ru>
2020-12-29 08:18:18 +0300

    algorithm SearchInsertPosition from LeetCode

---
+++ src/LeetCode/SearchInsertPosition.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+public class SearchInsertPosition {
+
+    public static void main(String[] args) {
+        int result = searchInsert(new int[] {1}, 0);
+        System.out.println(result);
+    }
+
+    private static int searchInsert(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] >= target) {
+                return i;
+            }
+            if (i == (nums.length - 1) && target > nums[i]){
+                return nums.length;
+            }
+        }
+        return 0;
+    }
+}
