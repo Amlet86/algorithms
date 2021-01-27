f97a314e77fbd2c0d3b85f68316f36857970e38b
Amlet86 <wecow@yandex.ru>
2021-01-27 08:05:18 +0300

    algorithm PascalTriangle2 from LeetCode

--- src/LeetCode/PascalTriangle.java
+++ src/LeetCode/PascalTriangle.java
@@ -1,34 +1,34 @@
 package LeetCode;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class PascalTriangle {
 
     public static void main(String[] args) {
-        getRow(4);
+        generate(5);
     }
 
-    private static List<Integer> getRow(int rowIndex) {
-        List<Integer> prevRow = new ArrayList<>();
-        prevRow.add(1);
-        if (rowIndex == 0)
-            return prevRow;
-        prevRow.add(1);
-        if (rowIndex == 1)
-            return prevRow;
-        for (int i = 2; i <= rowIndex; i++) {
-            List<Integer> thisRow = new ArrayList<>(Collections.singleton(1));
-            for (int j = 1; j < i; j++) {
-                int topLeft = prevRow.get(j - 1);
-                int topRight = prevRow.get(j);
-                thisRow.add(topLeft + topRight);
+    private static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (numRows == 0) return result;
+        List<Integer> previousList = new ArrayList<>();
+        previousList.add(1);
+        result.add(previousList);
+        for (int i = 2; i <= numRows; i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j <= previousList.size(); j++) {
+                if (j == 0) list.add(1);
+                else if (j == previousList.size()) list.add(1);
+                else {
+                    int tmp = previousList.get(j - 1) + previousList.get(j);
+                    list.add(j, tmp);
+                }
             }
-            thisRow.add(1);
-            prevRow = thisRow;
+            result.add(list);
+            previousList = list;
         }
-        return prevRow;
+        return result;
     }
 
 }
