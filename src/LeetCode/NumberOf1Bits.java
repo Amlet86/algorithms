218cb3e5b90edba3b85bbe6dcfe0012f667f9b62
Amlet86 <wecow@yandex.ru>
2021-01-28 08:20:59 +0300

    algorithm NumberOf1Bits from LeetCode

---
+++ src/LeetCode/NumberOf1Bits.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+public class NumberOf1Bits {
+
+    public static void main(String[] args) {
+        int result = hammingWeight(00000000000000000000000000001011);
+        System.out.println(result);
+    }
+
+    private static int hammingWeight(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++, n = n >> 1) {
+            if((n & 1) == 1) result++;
+        }
+        return result;
+    }
+
+}
