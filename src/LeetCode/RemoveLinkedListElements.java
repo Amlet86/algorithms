e978d1e6665528023b8314c59796a97aa80a5c7c
Amlet86 <wecow@yandex.ru>
2021-02-02 07:47:23 +0300

    algorithm RemoveLinkedListElements from LeetCode

---
+++ src/LeetCode/RemoveLinkedListElements.java
@@ -0,0 +1,44 @@
+package LeetCode;
+
+public class RemoveLinkedListElements {
+
+    public static void main(String[] args) {
+        removeElements(new ListNode(1, new ListNode(2, new ListNode(6, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6, null))))))), 6);
+    }
+
+    private static ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+
+        ListNode result = head;
+
+        while (result != null && result.next != null) {
+            if (result.next.val == val) {
+                result.next = result.next.next;
+            } else {
+                result = result.next;
+            }
+        }
+
+        return head;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
