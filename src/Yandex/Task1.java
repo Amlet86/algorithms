3b313962c2196895d0e3aa6e8741079a6999db5c
Amlet86 <wecow@yandex.ru>
2021-03-01 09:13:18 +0300

    yandex practice

---
+++ src/Yandex/Task1.java
@@ -0,0 +1,14 @@
+package Yandex;
+
+import java.util.Scanner;
+
+public class Task1
+{
+    public static void main(String[] args)
+    {
+        Scanner console = new Scanner(System.in);
+
+        System.out.print(console.nextInt() + console.nextInt());
+    }
+
+}
---
+++ src/Yandex/Task2.java
@@ -0,0 +1,29 @@
+package Yandex;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.io.PrintStream;
+import java.io.StreamTokenizer;
+
+public class Task2 {
+
+    public static void main(String[] args) throws IOException
+    {
+        StreamTokenizer in = new StreamTokenizer(
+            new BufferedReader(
+                new LineNumberReader(
+                    new FileReader("src/Yandex/input.txt"))));
+
+        PrintStream out = new PrintStream("src/Yandex/output.txt");
+
+        int a, b;
+        in.nextToken();
+        a = (int) in.nval;
+        in.nextToken();
+        b = (int) in.nval;
+        out.println(a + b);
+    }
+
+}
